2d40cdeb6da028317be0ffbaf014b4fb6b8a71b9
Rosalie Lee <rosalie337@gmail.com>
2021-03-05 14:41:07 -0800

    README.md (#5)
    * front end rendering simple form (#3)
    
    * front end rendering simple form
    
    * updated snaps
    
    * installed dependencies
    
    * created README.md
    
    * added API to README.md
    
    Co-authored-by: Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
    Co-authored-by: Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
-
-export default function App() {
-  return <h1>Hello World</h1>;
+import {
+  CardElement,
+  useStripe,
+  useElements
+} from '@stripe/react-stripe-js';
+function App() {
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+    fetch('http://localhost:7890/api/v1/charge', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ paymentMethod: paymentMethod.id })
+    })
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button>Submit</button>
+    </form>
+  );
 }
+export default App;
