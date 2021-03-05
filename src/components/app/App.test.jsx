090bb7339faeb6cf8e08adcfc24b70918482ed19
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-04 18:25:09 -0800

    front end rendering simple form (#3)
    * front end rendering simple form
    
    * updated snaps

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import {
+  Elements
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51IQLH1AlZQgGODSJBkoELcpjSB9liGgfLpot9mWQER1Cv9RCBpLnvhdrDPORukS3YQoeQ4OHq04EC95pX1GrnAHY00rW9sayk4');
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
+import {
+  Elements
+} from '@stripe/react-stripe-js';
+
+import { loadStripe } from '@stripe/stripe-js';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_51IQLH1AlZQgGODSJBkoELcpjSB9liGgfLpot9mWQER1Cv9RCBpLnvhdrDPORukS3YQoeQ4OHq04EC95pX1GrnAHY00rW9sayk4');
 
 render(
-  <App />,
+  
+  <Elements stripe={stripePromise}>
+    <App />
+  </Elements>,
   document.getElementById('root')
 );
