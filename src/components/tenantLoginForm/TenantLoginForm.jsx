2b415cb7448564bfe7d7136df7967e65f25debce
ErikGraciosa <erikgraciosa@gmail.com>
2021-03-07 12:06:07 -0800

    login state added

--- src/components/tenantLoginForm/TenantLoginForm.jsx
+++ src/components/tenantLoginForm/TenantLoginForm.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './TenantLoginForm.css';
 
-function TenantLoginForm() {
+function TenantLoginForm({ 
+  email,
+  password,
+  login,
+  onChangeEmail,
+  onChangePassword
+}) {
   return (
     <div>
       <p>Sample Text</p>
-      <form className={styles.loginForm}>
+      <form 
+        onSubmit={login}
+        className={styles.loginForm}>
         <label>Email</label>
         <input 
           type="plain/text"
           placeholder="Enter email as username"
+          value={email}
+          onChange={(event) => {onChangeEmail(event.target.value);}}
           required>
         </input>
         <label>Password</label>
         <input 
           type="password"
           placeholder="Type Password"
+          value={password}
+          onChange={(event) => {onChangePassword(event.target.value);}}
           required></input>
         <button>Log In</button>
       </form>
     </div>
   );
 }
+
+TenantLoginForm.propTypes = {
+  email: PropTypes.string,
+  password: PropTypes.string,
+  login: PropTypes.func.isRequired,
+  onChangeEmail: PropTypes.func.isRequired,
+  onChangePassword: PropTypes.func.isRequired,
+};
 
 export default TenantLoginForm;
--- src/containers/TenantLogin.jsx
+++ src/containers/TenantLogin.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TenantLoginForm from '../components/tenantLoginForm/TenantLoginForm';
 
 function TenantLogin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = (value) => {
+    setEmail(value);
+  };
+
+  const onChangePassword = (value) => {
+    setPassword(value);
+  };
+
+  const login = (event) => {
+    event.preventDefault();
+    //Change this to provide the email/password to the backend route 
+    console.log('login clicked');
+  };
+
+  console.log(email, password);
   return (
     <div>
-      <TenantLoginForm />
+      <TenantLoginForm
+        email={email}
+        password={password} 
+        login={login}
+        onChangeEmail={onChangeEmail}
+        onChangePassword={onChangePassword}/>
     </div>
   );
 }
