f822dcdccfc9482adc7bd24385cb22f96c9225f3
ErikGraciosa <erikgraciosa@gmail.com>
2021-03-08 15:09:37 -0800

    refactored the login

--- src/containers/TenantLogin.jsx
+++ src/containers/TenantLogin.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import TenantLoginForm from '../components/tenantLoginForm/TenantLoginForm';
 import Logo from '../components/logo/Logo';
-const URL = 'https://pool-party-staging.herokuapp.com';
+import { loginPost } from '../services/login.js';
 
 function TenantLogin() {
   const [email, setEmail] = useState('');
@@ -20,16 +20,7 @@ function TenantLogin() {
     event.preventDefault();
     //Change this to provide the email/password to the backend route 
     console.log('login clicked');
-    fetch(`${URL}/api/v1/auth/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ 
-        email,
-        password,
-      })
-    });
+    loginPost(email, password);
   };
 
   return (
