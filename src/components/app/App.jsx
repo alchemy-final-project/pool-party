4630b721a929763b5a617acd00e0a31ca4c84743
ErikGraciosa <erikgraciosa@gmail.com>
2021-03-08 10:01:43 -0800

    added placeholder pages

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,51 +1,25 @@
 import React from 'react';
-import {
-  CardElement,
-  useStripe,
-  useElements
-} from '@stripe/react-stripe-js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import OwnerSignUp from '../../containers/OwnerSignUp';
 import TenantLogin from '../../containers/TenantLogin';
 import TenantSignUp from '../../containers/TenantSignUp';
+import Payment from '../../containers/Payment';
+import Dashboard from '../../containers/Dashboard';
+import Header from '../header/Header';
 
 function App() {
-  const stripe = useStripe();
-  const elements = useElements();
-  const handleSubmit = async(event) => {
-    event.preventDefault();
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement),
-    });
-    fetch('http://localhost:7890/api/v1/charge', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ paymentMethod: paymentMethod.id })
-    });
-  };
-
   return (
     <>
-
+      <Header/>
       <Router>
         <Switch>
           <Route exact path="/" component={TenantLogin} />
           <Route exact path="/tenantSignup" component={TenantSignUp} />
           <Route exact path="/ownerSignup" component={OwnerSignUp} />
+          <Route exact path="/payment" component={Payment} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
       </Router>
-      <br/>
-      <hr/>
-      <br/>
-      <br/>
-      <form onSubmit={handleSubmit}>
-        <CardElement />
-        <button>Submit</button>
-      </form>
-      
     </>
   );
 }
---
+++ src/containers/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <p>TENANT DASHBOARD PAGE</p>
+      <p>TENANT DASHBOARD PAGE</p>
+      <p>TENANT DASHBOARD PAGE</p>
+      <p>TENANT DASHBOARD PAGE</p>
+      <button>Pay your bill</button>
+    </div>
+  );
+}
+
+export default Dashboard;
