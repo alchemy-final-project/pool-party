8903efead406d8d4e27d419445c25f67f499da7d
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-09 14:12:10 -0800

    Header/header.js (#19)
    * added header links --needs link to owner sign-up and logo
    
    * changed the h1 tag content
    
    Co-authored-by: Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
 
 function Header() {
+
   return (
-    <div>
-      <h1>POOL PARTY + Navigation</h1>
+    <div className="header">
+      <h1>POOL PARTY + Header</h1>
+      <img className="logo" src="" alt="" />
+      <ul className="header_links" >
+        <li><Link exact to="/">Home</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link>Owner Sign Up</Link></li>
+      </ul>
     </div>
   );
 }
