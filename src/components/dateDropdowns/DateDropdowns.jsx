c6d8eed034f29e5e320011901044ec280dac4f8e
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-10 14:22:23 -0800

    Feature/added payment dropdowns (#23)
    * commented out headers
    
    * fix test
    
    * changed cc post request
    
    * added dropdowns to payment, refactored payment into container
    
    * undid the hardcoded ownerId

---
+++ src/components/dateDropdowns/DateDropdowns.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function DateDropdowns({ onRentYearChange, onRentMonthChange }) {
+
+  const years = ['2021', '2022'];
+  const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  const yearsHtml = years.map(year =>
+    <option key={year} value={year}>{year}</option>
+  );
+
+  const monthsHtml = months.map(month =>
+    <option key={month} value={month}>{month}</option>
+  );
+
+  return (
+    <div>
+      <label>Month
+        <select onChange={(event) => onRentMonthChange(event.target.value)}>
+          {monthsHtml}
+        </select>
+      </label>
+      <label>Years
+        <select onChange={(event) => onRentYearChange(event.target.value)}>
+          {yearsHtml}
+        </select>
+      </label>
+    </div>
+  );
+}
+
+DateDropdowns.propTypes = {
+  onRentYearChange: PropTypes.func.isRequired,
+  onRentMonthChange: PropTypes.func.isRequired
+};
+
+export default DateDropdowns;
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CreditCardEntry from '../components/cardElement/CreditCardEntry';
+import DateDropdowns from '../components/dateDropdowns/DateDropdowns';
+import {
+  CardElement,
+  useStripe,
+  useElements
+} from '@stripe/react-stripe-js';
+import { post } from '../services/request';
 
 function Payment() {
+  const [rentYear, setRentYear] = useState('2021');
+  const [rentMonth, setRentMonth] = useState('1');
+ 
+  const onRentYearChange = (value) => {
+    setRentYear(value);
+  };
+
+  const onRentMonthChange = (value) => {
+    setRentMonth(value);
+  };
+
+  //Stripe Block
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+    post('/api/v1/transactions', {
+      paymentMethodId: paymentMethod.id,
+      rentYear,
+      rentMonth
+    });
+  };
+
   return (
     <div>
-      <p>CreditCard Entry Page</p>
-      <p>CreditCard Entry Page</p>
-      <p>CreditCard Entry Page</p>
-      <p>CreditCard Entry Page</p>
-      <CreditCardEntry/>
+      <h1>Entry Credit Card Here to Pay into the Party</h1>
+      <p>Need to have a get call to get the amount</p>
+      <p>/ Add the amount with a future get</p>
+      <DateDropdowns
+        onRentYearChange={onRentYearChange}
+        onRentMonthChange={onRentMonthChange}/>
+      <CreditCardEntry handleSubmit={handleSubmit}/>
     </div>
   );
 }
