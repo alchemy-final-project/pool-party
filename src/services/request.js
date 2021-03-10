35b3709dc22fe573de9b0b4223b657aa69371c89
ErikGraciosa <erikgraciosa@gmail.com>
2021-03-10 09:10:50 -0800

    commented out headers

--- src/services/request.js
+++ src/services/request.js
@@ -1,27 +1,27 @@
 const readHeaders = {};
 const writeHeaders = {
-    'Content-Type': 'application/json'
+  'Content-Type': 'application/json'
 };
 const headers = {
-    POST: writeHeaders,
-    GET: readHeaders,
-    PUT: writeHeaders,
-    DELETE: readHeaders
+  POST: writeHeaders,
+  GET: readHeaders,
+  PUT: writeHeaders,
+  DELETE: readHeaders
 };
 
 const request = (path, method, body) => {
-    return fetch(`${process.env.API_URL}${path}`, {
-        method,
-        headers: headers[method],
-        credentials: 'include',
-        body: body ? JSON.stringify(body) : null
-    })
-        .then(res => Promise.all([res.ok, res.json()]))
-        .then(([ok, json]) => {
-            if (!ok) throw json;
-            return json;
-        });
-}
+  return fetch(`${process.env.API_URL}${path}`, {
+    method,
+    headers: headers[method],
+    credentials: 'include',
+    body: body ? JSON.stringify(body) : null
+  })
+    .then(res => Promise.all([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+};
 
 export const post = (path, body) => request(path, 'POST', body);
 export const get = path => request(path, 'GET');
