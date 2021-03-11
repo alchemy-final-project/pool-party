2c84c4939cdead5090ae90011d0814d95d088d1f
kadycat <katieberrym@gmail.com>
2021-03-11 15:09:45 -0800

    added more styling to dashboard (#36)

--- src/components/paymentPool/RoomateList.jsx
+++ src/components/paymentPool/RoomateList.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './RoomateList.css';
+import beachball_balloon from '../../images/beachball_balloon.png';
 
 const RoomateList = ({ roomates }) => {
     const roomateElements = roomates.map(roomate => (
         <div className={styles.container}>
-            <li key={roomate.id}>
-                {roomate.name}
+            <img src={beachball_balloon} alt="beachball" />
+            <li key={roomate.id} >
+                {roomate.name}'s Monthly Cost Is
             </li >
 
-            <li key={roomate.id}>
+            <li key={roomate.id} style={{ color: "magenta", fontSize: "30px", fontWeight: "bold" }}>
                 ${roomate.monthlyCost}
             </li>
-
-        </div>
+        </div >
     ));
 
     return <ul className={styles.RoomateList} style={{ marginTop: "300px", minHeight: "300px" }}
