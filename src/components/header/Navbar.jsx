7b66491aab231975dae18e06fdaab9a1f90a3c68
Rosalie Lee <rosalie337@gmail.com>
2021-03-10 18:36:12 -0800

    Header/setup file structure (#28)
    * added header links --needs link to owner sign-up and logo
    
    * added icon to index.html  and changed header file structure
    
    * added Navbar.js and Navbar.css
    
    Co-authored-by: Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>

---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from '../header/Navbar.css';
+
+export default function Navbar() {
+  return ( 
+    <>     
+      <nav className={styles.navContainer}>
+        <div className={styles.logo}>
+          <NavLink to="/">Pool Party</NavLink>
+        </div>
+        <ul className={styles.navMenu}>
+          <li className={styles.navItem}>
+            <NavLink to="/aboutUs">About Us</NavLink>
+          </li>
+          <li className={styles.navItem}>
+            <NavLink to="/tenantSignup">Sign Up</NavLink>
+          </li>
+          <li className={styles.navItem}>
+            <NavLink to="/">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
