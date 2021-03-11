c839a034d02c49529ad133c667af66ea03040129
Katie <katieberrym@gmail.com>
2021-03-10 18:00:40 -0800

    added aboutUs basic structure and styling

--- src/containers/AboutUs.jsx
+++ src/containers/AboutUs.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
+import styles from './AboutUs.css';
+import img from '../images/beachball.jpg';
 
-function AboutUs() {
-  return (
-    <div>
-      <h1>ABOUT US PAGE</h1>
-      <h1>ABOUT US PAGE</h1>
-      <h1>ABOUT US PAGE</h1>
-      <h1>ABOUT US PAGE</h1>
-      <h1>ABOUT US PAGE</h1>
+const AboutUs = () => (
+  <div>
+    <div className={styles.AboutUs}>
+      <h1>About Us</h1>
+      <div className="container">
+        <img className="profile-pic" src={img} type='image' alt="beachball" />
+        <div className="name-text">
+
+          <h2 className="name">Rosalie Lee</h2>
+          <p className="about-text">
+            Rosalie is awesome!
+        </p>
+        </div>
+      </div>
+
+      <div className="container">
+        <img className="profile-pic" src={img} type='image' alt="beachball" />
+        <div className="name-text">
+
+          <h2 className="name">Erik Graciosa</h2>
+          <p className="about-text">
+            Erik is awesome!
+        </p>
+        </div>
+      </div>
+
+      <div className="container">
+        <img className="profile-pic" src={img} type='image' alt="beachball" />
+        <div className="name-text">
+
+          <h2 className="name">Katie Berry</h2>
+          <p className="about-text">
+            Katie is awesome!
+        </p>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default AboutUs;
