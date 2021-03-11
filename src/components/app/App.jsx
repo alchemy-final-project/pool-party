b464f3352566de8adb7b401e16127fd48515210e
Rosalie Lee <rosalie337@gmail.com>
2021-03-11 09:36:53 -0800

    Css/styling login 01 (#30)
    * added header links --needs link to owner sign-up and logo
    
    * styled home, footer and navbar components
    
    * removed imports from App.js and updated snapshot
    
    Co-authored-by: Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../home/Home';
 import OwnerSignUp from '../../containers/OwnerSignUp';
 import TenantLogin from '../../containers/TenantLogin';
 import TenantSignUp from '../../containers/TenantSignUp';
@@ -7,6 +8,7 @@ import Payment from '../../containers/Payment';
 import Dashboard from '../../containers/Dashboard';
 import AboutUs from '../../containers/AboutUs';
 import Navbar from '../header/Navbar';
+import Footer from '../footer/Footer';
 
 function App() {
 
@@ -15,13 +17,15 @@ function App() {
       <Router>
         <Navbar/>
         <Switch>
-          <Route exact path="/" component={TenantLogin} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={TenantLogin} />
           <Route exact path="/tenantSignup" component={TenantSignUp} />
           <Route exact path="/ownerSignup" component={OwnerSignUp} />
           <Route exact path="/payment" component={Payment} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/aboutUs" component={AboutUs} />
         </Switch>
+        <Footer/>
       </Router>
     </>
   );
