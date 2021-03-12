13d1dac41f2da6b210b791b853089e2b2aa9e740
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-11 17:58:41 -0800

    Dashboard state updates (#38)
    * dashboard-state-updates
    
    * updated snap
    
    * added logic to show who paid
    
    * updated merge conflicts, again

--- src/components/paymentPool/RoomateList.jsx
+++ src/components/paymentPool/RoomateList.jsx
@@ -3,24 +3,37 @@ import PropTypes from 'prop-types';
 import styles from './RoomateList.css';
 import beachball_balloon from '../../images/beachball_balloon.png';
 
-const RoomateList = ({ roomates }) => {
-  const roomateElements = roomates.map(roomate => (
-    <div className={styles.container}>
+const RoomateList = ({ roomates, transactions }) => {
+  console.log(transactions);
+  const roomateElements = transactions.map(transactions => (
+    <div key={transactions.id} className={styles.container}>
       <img src={beachball_balloon} alt="beachball" />
-      <li key={roomate.id} >
-        {roomate.name}'s Monthly Cost Is
+      <li key={transactions.id} >
+        {transactions.name}'s Monthly Cost Is
       </li >
-
-      <li key={roomate.id} style={{ color: 'magenta', fontSize: '30px', fontWeight: 'bold' }}>
-                ${roomate.monthlyCost}
+      <li
+        key={transactions.id}
+        style={{ color: 'magenta', fontSize: '30px', fontWeight: 'bold' }}>
+                ${transactions.monthly_cost}
+      </li>
+      <li>Paid? {transactions.payment_confirmed === true 
+        ? 'Payments Processed' 
+        : transactions.payment_confirmed != null
+          ? 'In Queue'
+          : 'NO'}
       </li>
     </div >
   ));
 
-  return <ul className={styles.RoomateList} style={{ marginTop: '300px', minHeight: '300px' }}
-    data-testid="roomates">
-    {roomateElements}
-  </ul>;
+  return (
+    
+    <ul className={styles.RoomateList} style={{ marginTop: '300px', minHeight: '300px' }}
+      data-testid="roomates">
+      <li className={styles.container}>Roommate Payment Status</li>
+      {roomateElements}
+    </ul>
+    
+  );
 };
 
 RoomateList.propTypes = {
@@ -30,7 +43,8 @@ RoomateList.propTypes = {
       name: PropTypes.string.isRequired,
       monthlyCost: PropTypes.number.isRequired
     }).isRequired
-  ).isRequired
+  ).isRequired,
+  transactions: PropTypes.array
 };
 
 export default RoomateList;
