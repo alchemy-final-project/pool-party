2f472840bf3237236dda21d6c482c960e24ea8f5
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-03-11 22:23:09 -0800

    snapshot updated and deleted imports

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import CreditCardEntry from '../components/cardElement/CreditCardEntry';
 import DateDropdowns from '../components/dateDropdowns/DateDropdowns';
 import {
   CardElement,
@@ -8,9 +7,7 @@ import {
   useElements
 } from '@stripe/react-stripe-js';
 import { post } from '../services/request';
-// import cardOptions from '../components/cardElement/CreditCardEntry';
 import styles from './Payment.css';
-import paymentBackground from '../images/water.jpg';
 
 const cardOptions = {
   iconStyle: 'solid',
